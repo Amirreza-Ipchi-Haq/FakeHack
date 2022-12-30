48452dbe9783d80754b828be1e741594a2b60260
Amirreza Ipchi-e-Haq <99384891+Amirreza-Ipchi-Haq@users.noreply.github.com>
2022-12-30 16:33:15 +0330

    Added green color to the outputted text

--- Hack.sh
+++ Hack.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo -en "\e[32m"
 for((i=0;i<101;i++))
 do
 	clear
-	echo -en "Accessing DataBase $i% complete...\n["
+	echo -en "Accessing the DataBase $i% complete...\n["
 	for((j=i;j;j--))
 	do
 		echo -n █
@@ -14,12 +15,12 @@ do
 	echo ]
 	sleep 0.1
 done
-echo "Accessed DataBase successfully!"
+echo "Accessed the DataBase successfully!"
 sleep 2
 for((i=0;i<101;i++))
 do
 	clear
-	echo -en "Accessing DataBase 100% complete...\n[████████████████████████████████████████████████████████████████████████████████████████████████████]\nAccessed DataBase successfully!\n\nCracking the code $i% complete...\n["
+	echo -en "Accessing the DataBase 100% complete...\n[████████████████████████████████████████████████████████████████████████████████████████████████████]\nAccessed the DataBase successfully!\n\nCracking the code $i% complete...\n["
 	for((j=i;j;j--))
 	do
 		echo -n █
@@ -37,5 +38,9 @@ echo -e "\nGranting access..."
 sleep 2
 echo "Access fully granted!"
 sleep 2
-echo -en "\n$1 has been hacked successfully!"
-xterm -T "Receiving Data" -n "Receiving Data" -e "while true;do shuf -zi 0-1 -n 1;done"
+case $1 in
+'')echo -en "\nThe device";;
+*)echo -en "\n$1";;
+esac
+echo -n " has been hacked successfully!"
+xterm -T "Receiving Data" -n "Receiving Data" -e "echo -en \"\e[0;32m\";while true;do shuf -zi 0-1 -n 1;done"
