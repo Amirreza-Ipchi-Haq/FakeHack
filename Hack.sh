b398fe4ba098c8318e86d13410683ffd0e34af79
Amirreza Ipchi-e-Haq <99384891+Amirreza-Ipchi-Haq@users.noreply.github.com>
2023-01-03 10:13:57 +0330

    Fixed most of the screen-blinking

--- Hack.sh
+++ Hack.sh
@@ -2,45 +2,19 @@
 echo -en "\e[32m"
 for((i=0;i<101;i++))
 do
-	clear
-	echo -en "Accessing the DataBase $i% complete...\n["
-	for((j=i;j;j--))
-	do
-		echo -n █
-	done
-	for((j=100-i;j;j--))
-	do
-		echo -n ' '
-	done
-	echo ]
-	sleep 0.1
+	echo -en "$(clear)Accessing the DataBase $i% complete...\n[$(for((j=i;j;j--));do echo -n █;done;for((j=100-i;j;j--));do echo -n ' ';done;echo ];sleep 0.1)\n"
 done
 echo "Accessed the DataBase successfully!"
 sleep 2
 for((i=0;i<101;i++))
 do
-	clear
-	echo -en "Accessing the DataBase 100% complete...\n[████████████████████████████████████████████████████████████████████████████████████████████████████]\nAccessed the DataBase successfully!\n\nCracking the code $i% complete...\n["
-	for((j=i;j;j--))
-	do
-		echo -n █
-	done
-	for((j=100-i;j;j--))
-	do
-		echo -n ' '
-	done
-	echo ]
-	sleep 0.1
+	echo -en "$(clear)Accessing the DataBase 100% complete...\n[████████████████████████████████████████████████████████████████████████████████████████████████████]\nAccessed the DataBase successfully!\n\nCracking the code $i% complete...\n[$(for((j=i;j;j--));do echo -n █;done;for((j=100-i;j;j--));do echo -n ' ';done;echo ];sleep 0.1)"
 done
-echo "Cracked the code successfully!"
+echo -e "\nCracked the code successfully!\n"
 sleep 0.5
-echo -e "\nGranting access..."
+echo Granting access...
 sleep 2
 echo "Access fully granted!"
 sleep 2
-case $1 in
-'')echo -en "\nThe device";;
-*)echo -en "\n$1";;
-esac
-echo -n " has been hacked successfully!"
+echo -e "\n$(case $1 in '')echo The device;;*)echo $1;;esac) has been hacked successfully!"
 xterm -T "Receiving Data" -n "Receiving Data" -e "echo -en \"\e[0;32m\";while true;do shuf -zi 0-1 -n 1;done"
